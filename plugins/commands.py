554f7c6c7114e57065240d01a5fdc3e90f33f2cf
filestoreapp <h4ckk6@gmail.com>
2025-07-08 10:24:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -125,7 +125,7 @@ async def start(client:Client, message):
 
     settings = await get_settings(int(data.split("_", 2)[1]))
     id = settings.get('fsub_id', AUTH_CHANNEL)
-    channel = int(id)
+    channel = int(id) if id is not None else None
     if settings.get('fsub_id', AUTH_CHANNEL) and not await is_subscribed(client, message.from_user.id, channel):
         invite_link = await client.create_chat_invite_link(channel)
         btn = [[
